7466d366aaa4154aaa6f812437af7e5728d29598
Rafaalex21 <rafaalepsantos94@gmail.com>
2022-11-21 16:12:02 -0300

    final version of the project, now using abstract class

--- bin/second_project.dart
+++ bin/second_project.dart
@@ -10,6 +10,24 @@ void main() {
   Fruta fruta01 = Fruta(nome, peso, cor, sabor, diasDesdeColheita);
   Fruta fruta02 = Fruta('uva', 30.4, 'roxa', 'azeda', 12);
   fruta01.estaMadura(30);
+  Legumes abobrinha = Legumes('Abobrinha', 'verde', 357, "salgada", true);
+  Legumes cenoura = Legumes('cenoura', 'laranja', 213, "salgada", false);
+  Citricas abacaxi = Citricas('pineapple', 890, 'amarelo', 'doce', 18, 2);
+  nozes castanha = nozes('Castanha', 72, 'marrom', 'salgado', 130, 5);
+  castanha.printAlimento();
+  abacaxi.printAlimento();
+  abobrinha.printAlimento();
+  cenoura.printAlimento();
+
+  abobrinha.cozinhar();
+  cenoura.cozinhar();
+
+  cenoura.separarIngredientes();
+  cenoura.fazerMassa();
+  cenoura.assar();
+  abacaxi.separarIngredientes();
+  abacaxi.fazerMassa();
+  abacaxi.assar();
 }
 
 class Alimento {
@@ -26,58 +44,109 @@ class Alimento {
   }
 }
 
-class Legumes extends Alimento {
+class Legumes extends Alimento implements bolo {
   bool isPrecisaCozinhar;
 
   Legumes(String nome, String cor, double peso, String sabor,
       this.isPrecisaCozinhar)
       : super(nome, cor, peso, sabor);
-  void cozinhar(){
-    if(isPrecisaCozinhar = true ){
-      print('Pronto o $nome está cozido');
-    }else{
+
+  void cozinhar() {
+    if (isPrecisaCozinhar) {
+      print('Pronto a(o) $nome está cozido');
+    } else {
       print('não é necessário cozinhar');
     }
   }
+
+  @override
+  void assar() {
+    print('assar a massa no forno');
+  }
+
+  @override
+  void fazerMassa() {
+    print('misturar ingredientes com farinha');
+  }
+
+  @override
+  void separarIngredientes() {
+    print('preparar e separar os ingredientes');
+  }
 }
 
-class Fruta extends Alimento {
-
+class Fruta extends Alimento implements bolo{
   int diasDesdeColheita;
   bool? isMadura;
 
-  Fruta(String nome, double peso, String cor, String sabor, this.diasDesdeColheita,
-      {this.isMadura}):super(nome,cor, peso, sabor);
+  Fruta(String nome, double peso, String cor, String sabor,
+      this.diasDesdeColheita,
+      {this.isMadura})
+      : super(nome, cor, peso, sabor);
 
   estaMadura(int diasParaMadura) {
     isMadura = diasDesdeColheita >= diasParaMadura;
     print(
         "A sua $nome foi colhida a $diasDesdeColheita dias, e precisa de $diasParaMadura para amadurecer");
   }
+
+  @override
+  void assar() {
+  print('assar a massa no forno');
+  }
+
+  @override
+  void fazerMassa() {
+  print('misturar ingredientes com farinha');
+  }
+
+  @override
+  void separarIngredientes() {
+  print('separar os ingredientes');
+  }
+
+
 }
 
-class Citricas {
-  String nome;
-  double peso;
-  String cor;
-  String sabor;
-  int diasDesdeColheita;
-  bool? isMadura;
+class Citricas extends Fruta implements bolo {
   double nivelAzedo;
 
-  Citricas(this.nome, this.cor, this.peso, this.sabor, this.diasDesdeColheita,
-      this.nivelAzedo,
-      {this.isMadura});
+  Citricas(String nome, double peso, String cor, String sabor,
+      int diasDesdeColheita, this.nivelAzedo,
+      {bool? isMadura})
+      : super(nome, peso, cor, sabor, diasDesdeColheita);
+  @override
+  void assar() {
+    super.assar();
+  }
+
+  @override
+  void fazerMassa() {
+    print('pegar a $nome,ovos, óleo, leite...');
+    super.fazerMassa();
+  }
+
+  @override
+  void separarIngredientes() {
+    print('pegar a $nome');
+    super.separarIngredientes();
+  }
 }
 
-class nozes {
-  String nome;
-  double peso;
-  String cor;
-  String sabor;
+class nozes extends Fruta {
   double nivelSeco;
 
-  nozes(this.nome, this.cor, this.peso, this.sabor, this.nivelSeco);
+  nozes(String nome, double peso, String cor, String sabor,
+      int diasDesdeColheita, this.nivelSeco)
+      : super(nome, peso, cor, sabor, diasDesdeColheita);
+}
+
+abstract class bolo {
+  void separarIngredientes();
+
+  void fazerMassa();
+
+  void assar();
 }
 
 funcDiasAteMadura(int dias) {
